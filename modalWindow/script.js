dfc33bab95b2f3b84f74965094bb5df3f46f2bd3
hermstefanny <aida.estefania.sosa@gmail.com>
2025-03-10 17:28:00 +0100

    Implement modal functionality with open/close events

--- modalWindow/script.js
+++ modalWindow/script.js
@@ -1,1 +1,28 @@
-'use strict';
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+console.log(btnsOpenModal);
+
+function showModalAndOverlay() {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+function hideModalAndOverlay() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", showModalAndOverlay);
+
+btnCloseModal.addEventListener("click", hideModalAndOverlay);
+overlay.addEventListener("click", hideModalAndOverlay);
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape" && !modal.classList.contains("hidden"))
+    hideModalAndOverlay();
+});
