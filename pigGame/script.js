5662d0ec9b1d964061c7ef9a713ce42cbec0a078
hermstefanny <aida.estefania.sosa@gmail.com>
2025-03-11 16:38:49 +0100

    Add game logic for dice rolling and player score management

--- pigGame/script.js
+++ pigGame/script.js
@@ -1,1 +1,37 @@
-'use strict';
+"use strict";
+//Selecting elements
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+const current0El = document.getElementById("current--0");
+
+let currentScore = 0;
+let activePlayer = 0;
+const score = [0, 0];
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add("hidden");
+
+//Dice Functionality
+btnRoll.addEventListener("click", function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  diceEl.classList.remove("hidden");
+
+  diceEl.src = `dice-${dice}.png`;
+  console.log(dice);
+
+  if (dice !== 1) {
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    current0El.textContent = currentScore;
+  } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+  }
+});
