51f600b291442f21267e981eee58adc9cae2992b
hermstefanny <aida.estefania.sosa@gmail.com>
2025-03-19 17:22:41 +0100

    Refactor game logic to initialize state and manage player switching

--- pigGame/script.js
+++ pigGame/script.js
@@ -8,32 +8,82 @@ const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 const player0El = document.querySelector(".player--0");
 const player1El = document.querySelector(".player--1");
+let current0El = document.getElementById("current--0");
+let current1El = document.getElementById("current--1");
 
-let currentScore = 0;
-let activePlayer = 0;
-const score = [0, 0];
+let score, currentScore, activePlayer, playing;
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+function init() {
+  playing = true;
+  currentScore = 0;
+  activePlayer = 0; //first player
+  score = [0, 0];
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+
+  currentScore = 0;
+  activePlayer = 0; //first player
+
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+}
+
+init();
+
+function switchPlayer() {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0; //if the active is 0, then the new active player will be 1, then if its 1, the next active player will be 0
+  currentScore = 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+}
 
 //Dice Functionality
 btnRoll.addEventListener("click", function () {
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  diceEl.classList.remove("hidden");
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6) + 1; // created as local variable because everytime we roll a dice, it delivers a new number
+    diceEl.classList.remove("hidden");
 
-  diceEl.src = `dice-${dice}.png`;
-  console.log(dice);
+    diceEl.src = `dice-${dice}.png`;
+    console.log(dice);
 
-  if (dice !== 1) {
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    currentScore = 0;
-    player0El.classList.toggle("player--active");
-    player1El.classList.toggle("player--active");
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
   }
 });
+
+btnHold.addEventListener("click", function () {
+  if (playing) {
+    score[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      score[activePlayer];
+
+    if (score[activePlayer] >= 30) {
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove("player--active");
+      diceEl.classList.add("hidden");
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener("click", init);
